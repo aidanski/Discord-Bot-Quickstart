cb1c0b05fda85fd531d4b9af078294301a036263
Malexion <Malexion@users.noreply.github.com>
2020-07-18 00:54:18 -0500

    Export types that IBotPlugin uses

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export * from './iteration';
 export * from './logger';
 export * from './console-reader';
 export * from './models';
+
+export * from 'discord.js';
+export { SuccessfulParsedMessage, ParsedMessage } from 'discord-command-parser';
+export { ParsedArgs } from 'minimist';
+export { Interface } from 'readline';
